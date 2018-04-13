1a0ecfec6d9854d32a641e270903450c78043074
test 2 <test>
2018-04-13 10:42:36 -0600

    fix chinese

--- chinese/lstm_seq2seq.py
+++ chinese/lstm_seq2seq.py
@@ -51,7 +51,7 @@ epochs = 100  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
 num_samples = 10000  # Number of samples to train on.
 # Path to the data txt file on disk.
-data_path = 'fra-eng/fra.txt'
+data_path = 'chi-eng/cmn.txt'
 
 # Vectorize the data.
 input_texts = []
